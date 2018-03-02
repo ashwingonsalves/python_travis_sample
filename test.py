d259a702cf6df82ea421ea39e6a5e641d4fef666
Ashwin Gonsalves <ashwing@Ashwins-MacBook-Pro-3.local>
2018-03-03 02:53:11 +0530

    Initial files added

---
+++ test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+desired_cap = {
+ 'browser': 'Chrome',
+ 'browser_version': '62.0',
+ 'os': 'Windows',
+ 'os_version': '10',
+ 'resolution': '1024x768'
+}
+
+driver = webdriver.Remote(
+    command_executor="http://" + os.environ['BROWSERSTACK_USER'] + ":" + os.environ['BROWSERSTACK_ACCESS_KEY'] + "@hub.browserstack.com:80/wd/hub",
+    desired_capabilities=desired_cap)
+
+driver.get("http://www.google.com")
+if not "Google" in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("BrowserStack")
+elem.submit()
+print driver.title
+driver.quit()
